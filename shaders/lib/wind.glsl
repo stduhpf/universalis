d0ef70ef51c73bae830098eb9d3434e632a3315b
stduhpf <stephduh@live.fr>
2020-02-28 20:33:19 +0100

    better wind

--- shaders/lib/wind.glsl
+++ shaders/lib/wind.glsl
@@ -1,5 +1,4 @@
 vec3 wind (vec3 pos){
-	float a =.15+.05*sin(frameTimeCounter*.05);
 	vec3 dirp = vec3(1.,-.01,1.4)*.1;
 
   vec3 dird = cos(frameTimeCounter*vec3(.99635256,1.01524,.345)*.5-pos*.1);
@@ -8,7 +7,11 @@ vec3 wind (vec3 pos){
   float speed = 1.5;
 
   float phase = speed*frameTimeCounter+dot(pos,dirp);
+	float a=.15+.05*sin(frameTimeCounter+phase*.2);
+
   float pressure = (sin(phase)*(1.+rainStrength)+.5*sin(phase*2.3015)+.18*cos(phase*3.953))/1.78;
 
+	phase = speed*frameTimeCounter+2.*dot(pos,dirp.xzy);
+	pressure = mix(pressure,(sin(phase)*(1.+rainStrength)+.5*sin(phase*2.3015)+.18*cos(phase*3.953))/1.78,.25);
 	return dird*pressure*pressure*a;
 }
