d07bf3492762f505ed0bf19af2b0e42512a4e5e9
stduhpf <stephduh@live.fr>
2020-04-23 17:48:56 +0200

    fixed negative ao

--- shaders/lib/normals.glsl
+++ shaders/lib/normals.glsl
@@ -2,7 +2,7 @@ vec4 nmp = gettex(normals,uv);
 vec3 nm = nmp.rgb*2.-1.;
 #if PBR_FORMAT ==labPBRv1_2
   vec2 tb = nm.xy;
-  ao = nm.z;
+  ao = nm.z*.5.5;
   n = vec3(tb,sqrt(1.-dot(tb,tb))); //test for 2 channels normals (is working fine)
 #else
 	ao = length(nm);
